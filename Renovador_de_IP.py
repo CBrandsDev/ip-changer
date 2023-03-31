683e2b2f8dcabed224e20cb65e18dc22bf01810c
CBrandsDev <cbrandao2001@gmail.com>
2023-03-31 18:35:18 -0300

    initial

---
+++ Renovador_de_IP.py
@@ -0,0 +1,85 @@
+import os
+import time
+import requests
+import socket
+import tkinter as tk
+import pystray
+from PIL import Image, ImageTk
+import threading
+import sys
+
+def getLocalIp():
+    return socket.gethostbyname(socket.gethostname())
+
+def getPublicIp():
+    response = requests.get('https://api.ipify.org')
+    return response.text
+
+def updateIpLabels():
+    localIpLabel.config(text="IP Local: " + getLocalIp())
+    publicIpLabel.config(text="IP Publico " + getPublicIp())
+
+def loopFunction():
+
+    activeLoop = activeLoop_var.get()
+    renewTime = int(renewTime_entry.get()) * 60
+
+    while activeLoop: 
+        os.system('ipconfig /release')
+        time.sleep(3)
+        os.system('ipconfig /renew')
+        time.sleep(renewTime)
+        updateIpLabels()
+        root.update()
+        
+def executeLoop():
+    threading.Thread(target=loopFunction, daemon=True).start()
+
+def onQuit():
+    root.quit()
+    root.destroy()
+    tray.stop()
+
+def toggleVisibility():
+    if root.state() == 'normal':
+        root.withdraw()
+    else:
+        root.deiconify()
+
+    return True
+
+def createSystray():
+    global tray
+    menu = (pystray.MenuItem('Abrir', toggleVisibility), pystray.MenuItem("Sair", onQuit))
+    tray = pystray.Icon("ip-renewal","IP Renewal", menu)
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.geometry("250x180")
+    root.title ("Renovador de IP")
+
+
+    renewTime_label = tk.Label(root, text="Tempo para mudar o IP(minutos):")
+    renewTime_label.pack()
+    renewTime_entry = tk.Entry(root)
+    renewTime_entry.pack()
+
+    activeLoop_var = tk.BooleanVar()
+    activeLoop_var.set(True)
+    activeLoop_checkbutton = tk.Checkbutton(root , text="Ativar Programa", variable=activeLoop_var)
+    activeLoop_checkbutton.pack()
+
+    startButton = tk.Button(root, text="Iniciar", command=executeLoop)
+    startButton.pack()
+
+    localIpLabel = tk.Label(root, text="IP local: ")
+    localIpLabel.pack()
+
+    publicIpLabel = tk.Label(root, text="IP público: ")
+    publicIpLabel.pack()
+
+    createSystray()
+    root.withdraw()
+    tray.run()
+    updateIpLabels()
+    root.mainloop()
